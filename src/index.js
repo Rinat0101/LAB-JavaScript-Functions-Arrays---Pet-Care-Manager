28ccbd687c6cf20a6153d4ef23d27d471cfb36a0
Anastasiia <anastasiaaaguseva@gmail.com>
2025-02-13 00:29:55 +0300

    starter code

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// 🐾 Pet Care Manager - Starter Code
+
+// TODO: Implement the Pet class
+
+// TODO: Implement the PetManager class
+
+// TODO: Handle UI interactions
+
+// Select elements
+const petListContainer = document.getElementById("pet-list");
+const addPetBtn = document.getElementById("add-pet-btn");
+
+// Add pet event listener
+addPetBtn.addEventListener("click", () => {
+    // TODO: Get input value and add a new pet
+});
+
+// TODO: Function to render pets in the UI
+
+// TODO: Functions to play with and feed pets
